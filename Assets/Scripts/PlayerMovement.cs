522cfdb30786051169fde6769e288c8aa1514dee
Woody <devkritt@gmail.com>
2026-05-07 04:26:16 +0700

    add : basic movement input

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private float moveSpeed = 5f;
     void Start()
     {
         
@@ -11,6 +11,9 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        Vector3 movement = new Vector3(xInput, 0f, yInput).normalized * Time.deltaTime * moveSpeed;
+        transform.Translate(movement);
     }
 }
